66fbab904b793898331d3eb74b8ac40602ed5e69
favor522 <gurin7772@yandex.com>
2024-02-26 11:35:35 +0300

    CPP

--- Lab2/Lab2/Lab2_1.cpp
+++ Lab2/Lab2/Lab2_1.cpp
@@ -8,21 +8,21 @@ int main(){
 	int c;
 	int min = 10;
 	for (int i = 0; i < 10; i++) {
-		A[i] =  rand() % 10;
-		cout << "Test of the massiv: " << A[i] << "\n";
-		if (A[i] < min) {
-			min = A[i];
+		*(A + i) = rand() % 10;
+		cout << "Test of the massiv: " << *(A + i) << "\n";
+		if (*(A + i) < min) {
+			min = *(A + i);
 			c = i;
 		}
 	}
 	cout << endl;
 	cout << "Minimal element: " << min << "\n";
 	cout << endl;
-	b = A[9];
-	A[c] = b;
-	A[9] = min;
+	b = *(A + 9);
+	*(A + c) = b;
+	*(A + 9) = min;
 	for (int i = 0; i < 10; i++) {
-		cout << "Test of the massiv: " << A[i] << "\n";
+		cout << "Test of the massiv: " << *(A + i) << "\n";
 	}
 	return 0;
 }
